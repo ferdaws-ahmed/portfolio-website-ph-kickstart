71a557c7e0bfba60771a8eab5634db6d2e9fede2
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2024-12-05 22:18:55 +0600

    project upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        montagu:['Montagu Slab', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
